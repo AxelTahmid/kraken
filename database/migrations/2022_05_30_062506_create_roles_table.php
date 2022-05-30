b1562a1a97a7ebbf1ee9ee54b7b454075a53c382
shahadat <shahadat@tikweb.dk>
2022-05-30 12:31:06 +0600

    Added role & permission migrations

--- database/migrations/2022_05_30_062506_create_roles_table.php
+++ database/migrations/2022_05_30_062506_create_roles_table.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
