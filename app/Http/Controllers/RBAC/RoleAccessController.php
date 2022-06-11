ec4165cd96ece5af699d29f2f6c509a9e2e79617
Axel Tahmid <axel.tahmid@gmail.com>
2022-06-12 00:56:29 +0600

    refactor: organized RoleAccessController

--- app/Http/Controllers/RBAC/RoleAccessController.php
+++ app/Http/Controllers/RBAC/RoleAccessController.php
@@ -25,72 +25,107 @@ class RoleAccessController extends Controller
             'role_slug' => 'required|string|max:191',
         ]);
 
-        $user = User::findOrFail($form_data['user_id']);
+        $user = User::with('roles')->findOrFail($form_data['user_id']);
         $role = Role::where('slug', $form_data['role_slug'])->firstOrFail();
 
         if ($form_data['_action'] == 'grant') {
-
-            if (!$user->hasRole($form_data['role_slug'])) {
-
-                $user->roles()->attach($role);
-                return $this->successResponse(
-                    User::with('roles')->findOrFail($user->id),
-                    $role->name . ' Privilege Granted.',
-                    201
-                );
-            };
-
-            return $this->errorResponse(
-                'User is already' . $role->name,
-                400
-            );
+            return $this->grant($user, $role, $form_data['role_slug']);
         }
 
         if ($form_data['_action'] == 'revoke') {
-
-            if ($user->hasRole($form_data['role_slug'])) {
-
-                $user->roles()->detach($role);
-                return $this->successResponse(
-                    User::with('roles')->findOrFail($user->id),
-                    $role->name . ' Privilege Revoked.',
-                );
-            }
-
-            return $this->errorResponse(
-                'User is not ' . $role->name,
-                400
-            );
+            return $this->revoke($user, $role, $form_data['role_slug']);
         }
 
         if ($form_data['_action'] == 'change') {
-
-            $user = User::with('roles')->findorFail($form_data['user_id']);
-            $prev_role_slug = !empty($user->roles[0]) && $user->roles[0]->slug ? $user->roles[0]->slug : null;
-
-            if (!$user->hasRole($form_data['role_slug'])) {
-
-                if ($prev_role_slug !== null) {
-                    $prev_role = Role::where('slug', $prev_role_slug)->firstOrFail();
-                    $user->roles()->detach($prev_role);
-                }
-
-                $user->roles()->attach($role);
-                return $this->successResponse(
-                    User::with('roles')->findOrFail($user->id),
-                    'Changed to ' . $role->name . ' Privilege.',
-                    201
-                );
-            }
-
-            return $this->errorResponse(
-                'User Already Has ' . $role->name . ' Role.',
-                400
-            );
+            return $this->change($user, $role, $form_data['role_slug']);
         }
 
         return $this->errorResponse(
             'Action Not Performed.',
+            400
+        );
+    }
+
+    /**
+     * Grant Role to a User.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Role  $role
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    protected function grant($user, $role, $slug)
+    {
+        if (!$user->hasRole($slug)) {
+
+            $user->roles()->attach($role);
+            return $this->successResponse(
+                User::with('roles')->findOrFail($user->id),
+                $role->name . ' Privilege Granted.',
+                201
+            );
+        };
+
+        return $this->errorResponse(
+            'User is already' . $role->name,
+            400
+        );
+    }
+
+    /**
+     * Revoke Role of a User.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Role  $role
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    protected function revoke($user, $role, $slug)
+    {
+        if ($user->hasRole($slug)) {
+
+            $user->roles()->detach($role);
+            return $this->successResponse(
+                User::with('roles')->findOrFail($user->id),
+                $role->name . ' Privilege Revoked.',
+            );
+        }
+
+        return $this->errorResponse(
+            'User is not ' . $role->name,
+            400
+        );
+    }
+
+    /**
+     * Change Role of a User.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Role  $role
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
+     */
+    protected function change($user, $role, $slug)
+    {
+        $prev_role_slug = !empty($user->roles[0]) && $user->roles[0]->slug ? $user->roles[0]->slug : null;
+
+        if (!$user->hasRole($slug)) {
+
+            if ($prev_role_slug !== null) {
+                $prev_role = Role::where('slug', $prev_role_slug)->firstOrFail();
+                $user->roles()->detach($prev_role);
+            }
+
+            $user->roles()->attach($role);
+            return $this->successResponse(
+                User::with('roles')->findOrFail($user->id),
+                'Changed to ' . $role->name . ' Privilege.',
+                201
+            );
+        }
+
+        return $this->errorResponse(
+            'User Already Has ' . $role->name . ' Role.',
             400
         );
     }
